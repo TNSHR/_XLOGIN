40e350aaca0d1525ed7b63cad286721c7410b16d
TNSHR <rohandw0@gmail.com>
2024-06-11 21:09:45 +0530

    final

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from "./login";
+
+
+
+function App() {
+  return (
+    
+    
+      <div className="App">
+        <Login/>
+      </div>
+   
+   
+  );
+}
+
+export default App;
---
+++ my-app/src/login.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+    validateForm(e.target.value, password);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    validateForm(username, e.target.value);
+  };
+
+  const validateForm = (username, password) => {
+    setIsFormValid(username !== '' && password !== '');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === 'user' && password === 'password') {
+      setIsLoggedIn(true);
+      setMessage('Welcome, user!');
+    } else {
+      setMessage('Invalid username or password');
+    }
+  };
+
+  return (
+    <div >
+<h1>Login Page</h1>
+      {isLoggedIn ? (
+        <div >{message}</div>
+      ) : (
+        <form  onSubmit={handleSubmit}>
+          <div><label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          /></div>
+
+          <div><label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          /></div>
+
+          <button type="submit" disabled={!isFormValid}>
+            Submit
+          </button>
+
+          {message && <div className="message">{message}</div>}
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default Login;
